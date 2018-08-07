f1b6d5ed531b81378e06a3b716bebdd7688e1cc2
andcat45 <nicholasathari@gmail.com>
2018-08-07 13:16:04 -0400

    0

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/iKen.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/iKen.java
@@ -17,14 +17,33 @@ public class iKen extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         waitForStart();
         runtime.reset();
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        thirdDrive = hardwareMap.get(DcMotor.class, "third_drive");
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "L");
+        rightDrive = hardwareMap.get(DcMotor.class, "R");
+        thirdDrive = hardwareMap.get(DcMotor.class, "M");
+
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         thirdDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        double leftPower;
+        double rightPower;
+        double thirdPower;
+
+
+
         while (opModeIsActive()) {
 
+            leftDrive.setPower(1);
+            rightDrive.setPower(1);
+            thirdDrive.setPower(1);
+
+            if(runtime.time()>2) {
+
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
+                thirdDrive.setPower(0);
+            }
 
         }
     }
