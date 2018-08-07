72892a17bea7a157d184a257ec2d364b201592ca
obscuredawn <jasondawn65@gmail.com>
2018-08-07 14:41:56 -0400

    gyro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+public class GyroDemo
+{public class iKen extends LinearOpMode {
+    public static void main(String[] args){
+
+        GyroSensor    gyro = new GyroSensor;
+        waitForStart();
+
+
+        while (opModeIsActive()){
+    }
+}
